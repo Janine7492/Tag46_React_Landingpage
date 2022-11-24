516d9b054b7372506ec37a0f0cdd83468bce3753
janine7492 <janine.supercode@gmail.com>
2022-11-24 18:55:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./components/header/Header";
+import FirstSection from './components/sections/firstSection/FirstSection';
+import SecondSection from './components/sections/secondSection/SecondSection';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <FirstSection />
+      <SecondSection />
+      <Footer />
     </div>
   );
 }
